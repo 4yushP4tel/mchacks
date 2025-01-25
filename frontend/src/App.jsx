08287105c2335e73bb2812b029496c4359ea5189
DavidP <171104270+davidpreda888@users.noreply.github.com>
2025-01-25 16:52:07 -0500

    Create patient admission page to list symptoms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
+import Admitted from './pages/Admitted';
 
 function App() {
   
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/signin' element={<Signin/>}></Route>
-        <Route path='/home' element={<Home/>}></Route>
-        <Route path='/patients_welcome' element={<Patients_welcome/>}></Route>
-        <Route path='/clinic_welcome' element={<Clinic_welcome/>}></Route>
-      </Routes>
-    </Router>
+    <Admitted/>
+    // <Router>
+    //   <Routes>
+    //     <Route path='/signin' element={<Signin/>}></Route>
+    //     <Route path='/home' element={<Home/>}></Route>
+    //     <Route path='/patients_welcome' element={<Patients_welcome/>}></Route>
+    //     <Route path='/clinic_welcome' element={<Clinic_welcome/>}></Route>
+    //   </Routes>
+    // </Router>
 
   )
 }
