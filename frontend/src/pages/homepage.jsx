7655ba297cf559eb3ba3f7f3b1e2e1d76999a7b3
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2025-01-25 16:25:29 -0500

    remove dot list style from header navbar

--- frontend/src/pages/homepage.jsx
+++ frontend/src/pages/homepage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header.jsx";
 import HomeBody from "./HomeBody.jsx";
 import Footer from "./Footer.jsx";
-import "./style.css";
+import "../style.css";
 
 function Homepage() {
     return (
