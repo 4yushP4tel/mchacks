1e9c6ec857c42221ba805705003fe4f9c101026c
Ayush <payush548@gmail.com>
2025-01-25 13:55:27 -0500

    added routes

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,9 @@
-from flask import Flask
+from flask import Flask, request, jsonify, session
 from openai import OpenAI
 
+app = Flask(__name__)
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
