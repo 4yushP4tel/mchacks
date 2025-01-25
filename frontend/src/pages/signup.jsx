6a2650866ba9db39b188d25a8405c3a7b1420d6d
Isaiah Abella <64171727+saia11@users.noreply.github.com>
2025-01-25 16:47:18 -0500

    non-styled signup page

---
+++ frontend/src/pages/signup.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+export default function SignupApp() {
+    
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        password: '',
+        age: '',
+    });
+
+    
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormData({[name]: value});
+    }
+
+    
+    function signup(event) {
+        event.preventDefault();
+        console.log("Form submitted", formData);
+        
+    }
+
+    return (
+        <>
+            <form onSubmit={signup}>
+                <div className="FullName">Enter your full name.</div>
+                <input
+                    type="text"
+                    name="fullName"
+                    id="fullName"
+                    placeholder="Full Name"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                />
+                
+                <div className="Email">Enter your email.</div>
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="youremail@email.com"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <div className="Password">Enter your password.</div>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                />
+
+                <div className="Age">Age</div>
+                <input
+                    type="text"
+                    name="age"
+                    id="age"
+                    value={formData.age}
+                    onChange={handleChange}
+                />
+                
+                
+                <button type="submit">Sign Up</button>
+            </form>
+        </>
+    );
+}
