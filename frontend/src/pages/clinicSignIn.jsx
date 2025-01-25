7d5d424d89843205f50eed3f27d8fbbcc704ae83
jmmyzhang <155680424+jmmyzhang@users.noreply.github.com>
2025-01-25 16:46:19 -0500

    change header

---
+++ frontend/src/pages/clinicSignIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header.jsx";
+import Footer from "../components/footer.jsx";
+import "../style.css";
+
+function ClinicSignIn() {
+    return (
+        <div className="app-container">
+            <Header />
+        </div>
+    )
+}
